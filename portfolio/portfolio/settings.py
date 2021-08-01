fdb28052cdb7f8d86d8e48fbb02f6bf83289c1a2
ishaanj2000 <58269543+ishaanj2000@users.noreply.github.com>
2021-08-01 21:56:14 +0530

    Update settings.py

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','sampledomain.com']
+ALLOWED_HOSTS = ['127.0.0.1','portfoliowebsite2021.herokuapp.com/']
 
 
 # Application definition
